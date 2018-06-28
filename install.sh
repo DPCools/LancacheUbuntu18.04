e38761ac0f92edb54011b922d273cd9012b27a78
root <root@lancache.fragland.co.uk>
2018-06-28 15:22:42 +0100

    some changes

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-apt-get install libgeoip-dev -y 
-apt-get install locate -y
-apt-get install net-tools -y
-apt-get install screen -y
-apt-get install checkinstall docbook-xsl docbook-xsl-ns docbook-xsl-doc-html xsltproc -y
-apt-get install nload iftop httpry iftop tcpdump tshark -y
-apt-get install autotools-dev cdbs debhelper dh-autoreconf dpkg-dev gettext libev-dev libpcre3-dev pkg-config software-properties-common autotools-dev cdbs debhelper dh-autoreconf dpkg-dev gettext libev-dev libpcre3-dev libudns-dev pkg-config fakeroot devscripts -y 
-apt-get install devscripts curl git unbound build-essential libpcre3 libpcre3-dev zlib1g-dev libreadline-dev libev4 libev-dev libncurses5-dev git libssl-dev -y
-apt-get install curl git unbound build-essential libpcre3 zlib1g-dev libreadline-dev libncurses5-dev libssl-dev httpry libudns0 libudns-dev libev4 libev-dev devscripts automake libtool autoconf autotools-dev cdbs debhelper dh-autoreconf dpkg-dev gettext pkg-config fakeroot libpcre3-dev libgeoip-dev -y
-apt-get install uuid-dev -y
+apt update
+apt upgrade -y
+apt install libgeoip-dev -y 
+apt install locate -y
+apt install net-tools -y
+apt install screen -y
+apt install checkinstall docbook-xsl docbook-xsl-ns docbook-xsl-doc-html xsltproc -y
+apt install nload iftop httpry iftop tcpdump tshark -y
+apt install autotools-dev cdbs debhelper dh-autoreconf dpkg-dev gettext libev-dev libpcre3-dev pkg-config software-properties-common autotools-dev cdbs debhelper dh-autoreconf dpkg-dev gettext libev-dev libpcre3-dev libudns-dev pkg-config fakeroot devscripts -y 
+apt install devscripts curl git unbound build-essential libpcre3 libpcre3-dev zlib1g-dev libreadline-dev libev4 libev-dev libncurses5-dev git libssl-dev -y
+apt install curl git unbound build-essential libpcre3 zlib1g-dev libreadline-dev libncurses5-dev libssl-dev httpry libudns0 libudns-dev libev4 libev-dev devscripts automake libtool autoconf autotools-dev cdbs debhelper dh-autoreconf dpkg-dev gettext pkg-config fakeroot libpcre3-dev libgeoip-dev -y
+apt install uuid-dev -y
 
 #get grub ready for old network settings
 sed -i -e 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
